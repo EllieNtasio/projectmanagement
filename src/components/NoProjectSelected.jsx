1286f07f694d444aa99486bda7d677fc4e6b1e0f
EllieNtasio <ellintasio@gmail.com>
2025-01-03 15:12:14 +0200

    v.2

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -4,10 +4,10 @@ export default function NoProjectSelected(){
     return <div className="mt-24 text-center  w-2/3">
         <img src={noProjectImage} alt="An empty task list" className="w-16 object-contain mx-auto"/>
         <h2 className="text-xl font-bold text-sky-950 my-4">No Project Selected</h2>
-        <p>Select a project or start a new one!</p>
-        <p>
-            <button>
-
+        <p className="text-navy  mb-4">Select a project or start a new one!</p>
+        <p className="mt-8">
+            <button className=" px-6 py-2 rounded-md bg-navy text-beige hover:bg-teal">
+                Create New Project
             </button>
         </p>
     </div>
